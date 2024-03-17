fe40b7c0824945132e95d90b3b8b029fb24175ff
Ihar Voitau <igorgrime@gmail.com>
2024-03-18 00:53:28 +0300

    feat: solve sellTickets task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -137,8 +137,19 @@ function makeWord(/* lettersObject */) {
  *    sellTickets([25, 25, 50]) => true
  *    sellTickets([25, 100]) => false (The seller does not have enough money to give change.)
  */
-function sellTickets(/* queue */) {
-  throw new Error('Not implemented');
+function sellTickets(queue) {
+  let cashRegister = 0;
+
+  for (let i = 0; i < queue.length; i += 1) {
+    if (queue[i] === 25) {
+      cashRegister += queue[i];
+    } else {
+      cashRegister -= queue[i] - 25;
+    }
+    if (cashRegister < 0) return false;
+  }
+
+  return true;
 }
 
 /**
